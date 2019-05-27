2cb9d01748eaedb1956f966209c94d66e24309c2
DanisovszkyMark <32265752+DanisovszkyMark@users.noreply.github.com>
2019-05-27 22:39:02 +0200

    Kliens elkezdése
    Elkészítve:
    
    -App.xaml a saját színekkel és témával [Elkezdve / Kész]
    -MainWindow [Kész]
    -LoginWindowView (dinamikus felület, kód vázának felépítése) [Elkezdve]
    -Mappaszerkezet kialakítása [Elkezdve / Kész]
    -ConnectionException [Elkezdve / Kész]

---
+++ client/WPF_client/WPF_client/LoginWindowView.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WPF_client.Exceptions;
+
+namespace WPF_client
+{
+    /// <summary>
+    /// A bejelentkezést teszi lehetővé.
+    /// A bejelentkezési adatok ellenőrzéséhez MSSQL szerverhez csatlakozik.
+    /// </summary>
+    public partial class LoginWindowView : Window
+    {
+        public LoginWindowView()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_login_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                connectToServer();
+
+                if (canLogin())
+                {
+                    //felületváltás és logolás
+                }
+                else
+                {
+                    //Hiba logolása, visszaüzenés
+                }
+            }
+            catch (ConnectionException ce)
+            {
+                //Hiba logolása, visszaüzenés
+            }
+            catch (Exception exc)
+            {
+                //Hiba logolása, visszaüzenés
+            }
+        }
+
+        private void connectToServer()
+        {
+            throw new ConnectionException();
+        }
+
+        private bool canLogin()
+        {
+            return false;
+        }
+    }
+}
---
+++ client/WPF_client/WPF_client/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace WPF_client
+{
+    /// <summary>
+    /// 3 másodpercig szimulált töltőképernyő után megnyílik a bejelentkező felület
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private DispatcherTimer timer = new DispatcherTimer();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            timer.Interval = new TimeSpan(0, 0, 3);
+            timer.Start();
+            timer.Tick += Timer_Tick;
+
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            timer.Stop();
+
+            LoginWindowView view = new LoginWindowView();
+            view.Show();
+            this.Close();
+        }
+    }
+}
