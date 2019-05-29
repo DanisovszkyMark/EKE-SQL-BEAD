134597698b31e06ba388f171a203a85a00da8e8f
DanisovszkyMark <32265752+DanisovszkyMark@users.noreply.github.com>
2019-05-29 11:21:34 +0200

    kliens - adatbázis kapcsolat előkészítése
    Elvégzett feladatok: A kliensben az adatbáziskapcsolathoz szükséges objektumok létrehozása. A kapcsolathoz szükséges adatokat az app.config állomány tartalmazza.
    
    Érintett objektumok:
    -BaseDatabaseManager
    -Record
    -app.config

---
+++ client/WPF_client/WPF_client/DatabaseManagers/BaseDatabaseManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_client.DatabaseManagers
+{
+    /// <summary>
+    /// Ellátja az datbáziskapcsolat menedzsmentjét az app.config file alapján.
+    /// </summary>
+    class BaseDatabaseManager
+    {
+        protected SqlConnection getConnection()
+        {
+            SqlConnection connection = new SqlConnection();
+            connection.ConnectionString = ConfigurationManager.ConnectionStrings["EKE-DB"].ConnectionString;
+            connection.Open();
+
+            return connection;
+        }
+
+        protected BaseDatabaseManager()
+        {
+        }
+    }
+}
---
+++ client/WPF_client/WPF_client/DatabaseManagers/Records/Record.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_client.DatabaseManagers.Records
+{
+    /// <summary>
+    /// A rekordoknak ad egy közös őst.
+    /// </summary>
+    class Record
+    {
+        private readonly int? id;
+        public int? Id
+        {
+            get { return id; }
+        }
+
+        protected bool deleted;
+        public bool Deleted
+        {
+            get { return deleted; }
+            set
+            {
+                if (value == true && deleted) throw new Exception("Already deleted record!");
+                deleted = value;
+            }
+        }
+
+        public Record(int? id)
+        {
+            if (id == null) throw new ArgumentNullException();
+
+            this.id = id;
+        }
+
+        protected Record()
+        {
+            deleted = false;
+        }
+    }
+}
