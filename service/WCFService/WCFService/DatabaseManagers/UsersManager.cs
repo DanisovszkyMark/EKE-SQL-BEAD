a94e5509c5942e6b8a9035d7f94521c1f31221c9
DanisovszkyMark <32265752+DanisovszkyMark@users.noreply.github.com>
2019-06-14 01:24:15 +0200

    Szolgáltatások - Felhasználók kezelése tárolt eljárásokon keresztül
    Elvégzett módosítások: A szolgáltatások a következő esetekben már az SQL szerveren található tárolt eljárásokat használják:
    -Rekord felvétele (Insert)
    -Rekord módosítása(Update)
    -Rekord törlése(Delete)
    
    Továbbá a CanLogin-t is a szerver kezeli.

--- service/WCFService/WCFService/DatabaseManagers/UsersManager.cs
+++ service/WCFService/WCFService/DatabaseManagers/UsersManager.cs
@@ -40,75 +40,44 @@ namespace WCFService.DatabaseManagers
 
         public void Login(string username)
         {
-            SqlCommand command = new SqlCommand();
-            command.CommandType = System.Data.CommandType.Text;
-            command.CommandText = @"UPDATE Users 
-                                    SET logged = @logged
-                                    WHERE username = @_username";
+            SqlCommand cmd = new SqlCommand();
 
-            SqlParameter _username = new SqlParameter();
-            _username.ParameterName = "@_username";
-            _username.SqlDbType = System.Data.SqlDbType.VarChar;
-            _username.Direction = System.Data.ParameterDirection.Input;
-            _username.Value = username;
-            command.Parameters.Add(_username);
+            cmd.CommandText = "Login";
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@username", username);
 
-            SqlParameter logged = new SqlParameter();
-            logged.ParameterName = "@logged";
-            logged.SqlDbType = System.Data.SqlDbType.Bit;
-            logged.Direction = System.Data.ParameterDirection.Input;
-            logged.Value = 1;
-            command.Parameters.Add(logged);
-
-            try { command.Connection = getConnection(); }
+            try { cmd.Connection = getConnection(); }
             catch (Exception)
             {
                 throw new DatabaseConnectionException();
             }
 
-            try { command.ExecuteNonQuery(); }
+            try { cmd.ExecuteNonQuery(); }
             catch (Exception)
             {
                 throw new DatabaseCommandTextException();
             }
-            command.Connection.Close();
         }
 
         public void Logout(string username)
         {
-            SqlCommand command = new SqlCommand();
-            command.CommandType = System.Data.CommandType.Text;
-            command.CommandText = @"UPDATE Users 
-                                    SET logged = @logged
-                                    WHERE username = @_username";
+            SqlCommand cmd = new SqlCommand();
 
-            SqlParameter _username = new SqlParameter();
-            _username.ParameterName = "@_username";
-            _username.SqlDbType = System.Data.SqlDbType.VarChar;
-            _username.Direction = System.Data.ParameterDirection.Input;
-            _username.Value = username;
-            command.Parameters.Add(_username);
+            cmd.CommandText = "Logout";
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@username", username);
 
-            SqlParameter logged = new SqlParameter();
-            logged.ParameterName = "@logged";
-            logged.SqlDbType = System.Data.SqlDbType.Bit;
-            logged.Direction = System.Data.ParameterDirection.Input;
-            logged.Value = 0;
-            command.Parameters.Add(logged);
-
-            try { command.Connection = getConnection(); }
+            try { cmd.Connection = getConnection(); }
             catch (Exception)
             {
                 throw new DatabaseConnectionException();
             }
 
-            try { command.ExecuteNonQuery(); }
+            try { cmd.ExecuteNonQuery(); }
             catch (Exception)
             {
                 throw new DatabaseCommandTextException();
             }
-
-            command.Connection.Close();
         }
     }
 }
